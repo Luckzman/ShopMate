88e65bbc783a94e1a5c29c5a0050f091ca05e324
luckzman <lumpsey@gmail.com>
2019-09-21 22:03:41 +0100

    implement design for homepage layout

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import TopNav from '../../component/presentation/TopNav';
 import NavBar from '../../component/presentation/NavBar';
-// import FilterSideBar from '../../component/presentation/FilterSideBar';
+import FilterSideBar from '../../component/presentation/FilterSideBar';
 // import ItemCard from '../../component/presentation/ItemCard';
 
 class Home extends Component {
@@ -10,6 +10,10 @@ class Home extends Component {
       <div>
         <TopNav />
         <NavBar />
+        <div className="container homepage">
+          <FilterSideBar className="filter-side-bar" />
+          
+        </div>
       </div>
     )
   }
